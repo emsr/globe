51d807b069caf861d4804992a82fd740f921f69c
Ed Smith-Rowland <3dw4rd@verizon.net>
2023-12-14 12:56:07 -0500

    Original files.

---
+++ globe.f
@@ -0,0 +1,155 @@
+      program globe
+c*****************************************************************
+c  This is a program to test extraction of the GLOBE data base.
+c  It is written in Fortran and there may need to be modifications
+c  depending on the Fortran compiler used and computer system.
+c  PCs and unix handle binary data differently, and therefore
+c  a few lines of code need to be changed depending on your computer
+c  system.  It is also assumed that the GLOBE data base has been
+c  placed on your computer system.
+c  Since PCs & unix use different directory separators
+c  (e.g. / vs \), you must be aware of that difference also.
+c
+c  These routines have been developed for GLOBE Version 1.0.
+c  This was a significant change from GLOBE Version 0.5.
+c  The Globe web site is:
+c      http://www.ngdc.noaa.gov/seg/topo/globe.shtml
+c  This site will allow you to download the Globe data free
+c  or you can order CDs containing the data.
+c  The data exists as 16 tiles and must be gunziped to be usable.
+c  The unzipped 16 files will require about 2GBytes of disk storage.
+c  Make sure when the files are unzipped, the file names are of
+c  the form: ?10g. There should ne NO SUFFIX. As future versions
+c  of Globe are released, the file name may change (e.g. a11g
+c  would be Globe version 1.1).  If you obtain such a file in the future,
+c  you will need to modify the file name in the file globe.dat below,
+c  or rename the file to its original name (e.g. a10g).
+c
+c  In addition to the 16 files (tiles) that contain the elevation
+c  data, the following data must exist in the file named globe.dat
+c  and should reside in the same directory as the GLOBE data files.
+c  The record with the headings should be the first record in the file.
+c*******************************************************************
+c  file lat1 lat2 lon1 lon2 Description
+c  a10g   50   90  180  270 Tile A lat(50-90N) lon(180- 90W)
+c  b10g   50   90  270  360 Tile B lat(50-90N) lon( 90-  0W)
+c  c10g   50   90    0   90 Tile C lat(50-90N) lon(  0- 90E)
+c  d10g   50   90   90  180 Tile D lat(50-90N) lon( 90-180E)
+c  e10g    0   50  180  270 Tile E lat( 0-50N) lon(180- 90W)
+c  f10g    0   50  270  360 Tile F lat( 0-50N) lon( 90-  0W)
+c  g10g    0   50    0   90 Tile G lat( 0-50N) lon(  0- 90E)
+c  h10g    0   50   90  180 Tile H lat( 0-50N) lon( 90-180E)
+c  i10g  -50    0  180  270 Tile I lat(50S-0 ) lon(180- 90W)
+c  j10g  -50    0  270  360 Tile J lat(50S-0 ) lon( 90-  0W)
+c  k10g  -50    0    0   90 Tile K lat(50S-0 ) lon(  0- 90E)
+c  l10g  -50    0   90  180 Tile L lat(50S-0 ) lon( 90-180E)
+c  m10g  -90  -50  180  270 Tile M lat(90-50S) lon(180- 90W)
+c  n10g  -90  -50  270  360 Tile N lat(90-50S) lon( 90-  0W)
+c  o10g  -90  -50    0   90 Tile O lat(90-50S) lon(  0- 90E)
+c  p10g  -90  -50   90  180 Tile P lat(90-50S) lon( 90-180E)
+c*******************************************************************
+c  You MUST remove the initial 3 columns from the data above.
+c  The file name should appear in column 1.
+c  As you obtain new versions of the Globe database, you may need
+c  to change the file names within this data file.
+c  This data file contains the 4 character names of the 16 tiles.
+c*******************************************************************
+c*******************************************************************
+c  4 Fortran source files make up this test program.
+c    globe.f         - the main program
+c    get_GLOBE_pfl.f - the subroutine that extracts the profile from
+c                      the GLOBE database. This file also contains
+c                      the subroutine DAZEL that is used to calculate
+c                      the points along a great circle path between
+c                      the 2 points given.
+c    subsunix.f      - the GLOBE extractions subroutines for unix systems.
+c    subspc.f        - the GLOBE extractions subroutines for PC systems.
+c
+c  For all system:
+c   1. Obtain the Globe data base and place the 16 tile files into
+c      a directory on your system.
+c   2. Place the file globe.dat from my web site into the same directory.
+c
+c  For unix systems:
+c   3. Modify subsunix.f to define the GLOBE data directory.
+c      This is contained in the 3 lines:
+c          path='/disc3/terrain2/globe/'  !  directory containing GLOBE data
+c          nchp=22                        !  # characters in path
+c          lu_globe=61                    !  FORTRAN unit number to use
+c   4. Combine the source code files:
+c          globe.f get_GLOBE_pfl.f subsunix.f
+c   5. Compile and link using your FORTRAN compiler.
+c      This has been tested on HP-UX with HP's FORTRAN/9000.
+c   6. Execute and input the recommended data to compare with the results
+c      you should obtain.
+c                      
+c  For PC systems:
+c   3. Modify subspc.f to define the GLOBE data directory.
+c      This is contained in the 3 lines:
+c          path='E:\globe\'               !  directory containing GLOBE data
+c          nchp=9                         !  # characters in path
+c          lu_globe=61                    !  FORTRAN unit number to use
+c   4. Combine the source code files:
+c          globe.f get_GLOBE_pfl.f subsunix.f
+c   5. Compile and link using your FORTRAN compiler.
+c      This has been tested using Salford FTN77.
+c   6. Execute and input the recommended data to compare with the results
+c      you should obtain.
+c
+c*******************************************************************
+c*******************************************************************
+c
+c  This code was written by Greg Hand from the U.S. Department of
+c  Commerce NTIA/ITS in Boulder, Colorado.  April 1999.
+c  It is freely available, but do not remove the comments.
+c  Email:gh@its.bldrdoc.gov
+c*****************************************************************
+      common /C_pfl/ pfl(5003)
+      dimension sample(101)
+      data sample/
+     + 1585.,1593.,1597.,1595.,1589.,1573.,1557.,1545.,1534.,1532.,
+     + 1526.,1508.,1492.,1486.,1481.,1477.,1477.,1486.,1497.,1506.,
+     + 1519.,1528.,1534.,1532.,1528.,1535.,1554.,1551.,1524.,1500.,
+     + 1494.,1509.,1523.,1538.,1554.,1540.,1518.,1517.,1497.,1484.,
+     + 1496.,1503.,1505.,1523.,1524.,1517.,1500.,1496.,1479.,1481.,
+     + 1487.,1485.,1482.,1480.,1480.,1490.,1494.,1497.,1504.,1511.,
+     + 1520.,1523.,1531.,1539.,1546.,1552.,1564.,1567.,1577.,1580.,
+     + 1586.,1593.,1599.,1605.,1620.,1628.,1637.,1641.,1651.,1662.,
+     + 1671.,1679.,1683.,1694.,1705.,1715.,1724.,1733.,1744.,1749.,
+     + 1764.,1803.,1839.,1839.,1860.,1871.,1890.,1900.,1916.,1943.,
+     + 1975./
+      Tx_lat=40.25         !  Transmitter Latitude
+      Tx_lon=-105.         !  Transmitter Longitude
+      Rx_lat=41.25         !  Receiver    Latitude
+      Rx_lon=-105.         !  Receiver    Longitude
+      npoints=101          !  # of points to extract
+      tlat=Tx_lat
+      tlon=Tx_lon
+      rlat=Rx_lat
+      rlon=Rx_lon
+      write(*,*) 'Sample input: 101, 40, -105, 41, -105'
+      write(*,*) 'Should produce the results:'
+      write(*,101) 101,1111.77
+      write(*,102)
+      write(*,103) sample
+      write(*,*) 'Please compare your results to those above.'
+      write(*,*)
+10    continue
+      write(*,'('' Enter npoints,T_lat,T_lon,R_lat,R_lon='',$)')
+      read(*,*,end=999,err=999) npoints,tlat,tlon,rlat,rlon
+      call get_GLOBE_pfl(tlat,tlon,rlat,rlon,npoints,pfl,*900)
+      write(*,101) nint(pfl(1)),pfl(2)
+101   format(i5,' points found, delta between points=',f10.2,' meters')
+      write(*,102)
+102   format(' Elevation profile in meters:')
+      write(*,103) (pfl(i+2),i=1,npoints)
+103   format(10f7.0)
+ccc      write(15,104) (i-1,float(i-1),pfl(i+2),i=1,npoints)
+ccc104   format(3(i5,f10.2,f9.0,2x))
+      go to 10
+900   write(*,901)
+901   format('Error in get_GLOBE_pfl')
+      go to 10
+999   continue
+      END
+c---------------------------------------------------------------------
---
+++ subspc.f
@@ -0,0 +1,210 @@
+      function GLOBE_elevation(xxlon,xxlat)
+c************************************************************
+c          extract the GLOBE elevation for (xxlon,xxlat)
+c          GLOBE_elevation= elevation in meters of point
+c                         = < -500 = file does not exist
+c                           This should only happen if your data files
+c                           are not in the directory specified by path.
+c          NOTE: GLOBE flags ocean values as -500.
+c                These routines change any -500 value to 0.
+c                If you wish to identify ocean values, you should modify
+c                the code in get_GLOBE_data to suit your needs.
+c************************************************************
+c          The elevation of the 4 points that contain the 
+c          (xxlon,xxlat) are found and the elevation is interpolated.
+c          The 4 points are:
+c                   2   3
+c                   1   4
+c************************************************************
+c          In order to get the same value at Latitude=-90
+c          regardless of longitude, any Latitude below -89.99167
+c          has been forced to = 2777 meters elevation.
+c          This is because the lowest latitude data record
+c          corresponds to latitude=-89.9916666666...,
+c          which is NOT the South Pole, and the values at
+c          different longitude are slightly different.
+c************************************************************
+      common /C_GLOBE_init/ lu_globe,path,nchp,tiles(16)
+	 character path*60,tiles*4
+      dimension z(4)
+      save ionce
+      data ionce/0/
+c*************************************************************
+      if(ionce.eq.0) then     !  read in names of the Globe data base files
+c          set directory path location to GLOBE data base
+         path='E:\GLOBE\'        !  (PC systems use '\')
+	 nchp=9               !  number of characters in path
+	 lu_globe=61          !  FORTRAN unit number used to OPEN Globe files
+	 open(lu_globe,file=path(1:nchp)//'globe.dat',
+     +         status='old',iostat=ios,err=920)
+	 rewind(lu_globe)
+	 read(lu_globe,'(a)') tiles(1)
+         do 10 i=1,16         !  read data for the 16 tiles
+10       read(lu_globe,'(a)') tiles(i)
+	 close(lu_globe)
+	 ionce=1
+      end if
+c*************************************************************
+      if(xxlat.lt.-89.99167) then       !  force South Pole = 2777 meters
+	 GLOBE_elevation=2777
+	 return
+      end if
+      xlon=xxlon
+      if(xlon.lt.0.) xlon=xlon+360.     !  make xlon [0 - 360]
+      xlat=xxlat
+      call GLOBE_index(xlat,xlon,ix,iy,dx,dy)    !  get (ix,iy) cell that contains (xlat,xlon)
+      call GLOBE_record(ix,iy,itile,irec)
+      call get_GLOBE_data(itile,irec,z(1),*900)  !  get elevation of point #1
+      iy2=iy+1                          !  move 1 cell north
+      if(iy2.gt.21599) iy2=21599        !  maximum cell north
+      call GLOBE_record(ix,iy2,itile,irec)
+      call get_GLOBE_data(itile,irec,z(2),*900)  !  get elevation of point #2
+      ix2=ix+1                          !  move 1 cell east
+      if(ix2.gt.43199) ix2=0            !  wrap around
+      call GLOBE_record(ix2,iy2,itile,irec)
+      call get_GLOBE_data(itile,irec,z(3),*900)  !  get elevation of point #3
+      call GLOBE_record(ix2,iy,itile,irec)
+      call get_GLOBE_data(itile,irec,z(4),*900)  !  get elevation of point #4
+      elevation=GLOBE_interp(dx,dy,z)   !  interpolate to find elevation at (xlat,xlon)
+      go to 910
+c          file does not exist
+900   elevation =-501.                  !  flag invalid elevation value
+910   GLOBE_elevation=nint(elevation)
+      return
+920   write(*,921) ios,path(1:nchp)//'globe.dat'
+921   format(' OPEN error=',i5,' file=',a)
+      stop 'OPEN error in  GLOBE_elevation'
+      end
+c-----------------------------------------------------------------
+      subroutine get_GLOBE_data(itile,irec,elev,*)
+c**********************************************************************
+c          Look in this routine for changes you need to make depending
+c          on your computer system.  PCs do not need byte swaping,
+c          while unix systems do.  Macs are not known.
+c**********************************************************************
+c          open a GLOBE data file and get the elevation corresponding
+c          to a particular cell corner.
+c
+c          itile = tile number of data file [1 to 16]
+c          irec  = record number of the cell to read
+c          elev  = returned elevation value in meters.
+c                  The GLOBE database contains -500 to signify ocean.
+c                  That value is converted to 0.
+c                  If you wish to do something different, do so in the routine.
+c**********************************************************************
+      common /C_GLOBE_init/ lu_globe,path,nchp,tiles(16)
+	 character path*60,tiles*4
+      integer*2 data
+      integer*4 byte1,byte2
+      save last_tile
+      data last_tile/0/
+c********************************************************************
+c          do we need to open a new data file?
+      if(last_tile.ne.itile) then       !  open a new GLOBE file
+         if(last_tile.ne.0) close(lu_globe)
+         open(lu_globe,file=path(1:nchp)//tiles(itile),
+     +        access='direct',recl=2,status='old',iostat=ios,err=900)
+	 last_tile=itile
+      end if
+c********************************************************************
+      read(lu_globe,rec=irec,err=910) data
+ccc      write(*,'('' data='',i10)') data
+c***********************************************************
+c***********************************************************
+c          The following line should be commented when you change the
+c          appropriate lines below.
+c      stop 'You MUST fix subroutine get_GLOBE_data for your system!'
+c***********************************************************
+c          For PC systems, 'data' is OK
+c***********************************************************
+c***********************************************************
+c          For unix systems, 'data' MUST be byte swapped
+c          The following lines should be uncommented for unix systems.
+c***********************************************************
+cc      byte1=iand(data,255)                !  byte swap for unix systems
+cc      byte2=ibits(data,8,8)               !  extract the upper 8 bits
+cxxx      idata32=data                    !  if your FORTRAN does not have the
+cxxx      byte2=0                         !  function ibits, use mvbits
+cxxx      call mvbits(idata32,8,8,byte2,0)
+cc      data=byte1*256 + byte2              !  byte swap
+c***********************************************************
+      if(data.eq.-500) data=0             !  ocean = 0
+      elev=data
+c***********************************************************
+c***********************************************************
+      return
+900   last_tile=0
+      write(*,901) ios,path(1:nchp)//tiles(itile)
+901   format(' OPEN error=',i5,' file=',a)
+      return 1
+910   continue
+      write(*,'(''Filename='',a,''  irec='',i10)') tiles(itile),irec
+      stop 'Should not get here in get_GLOBE_data'
+      end
+c-----------------------------------------------------------------
+      subroutine GLOBE_record(ix,iy,itile,irec)
+c          Given: (ix,iy) - the Globe cell location
+c          Find:  itile - tile index containing (ix,iy)
+c                 irec  - record number within itile containing elevation
+      icol=mod(ix,10800)
+      jx=ix/10800 + 1
+      if(iy.ge.16800) then
+	 jy=0                              !  Tiles ABCD
+	 irow=21600-iy
+      else if(iy.ge.10800) then
+	 jy=1                              !  Tiles EFGH
+	 irow=16800-iy
+      else if(iy.ge. 4800) then
+	 jy=2                              !  Tiles IJKL
+	 irow=10800-iy
+      else if(iy.ge.    0) then
+	 jy=3                              !  Tiles MNOP
+	 irow=4800-iy
+      end if
+      itile=jx + jy*4
+      irec=(irow-1)*10800 + icol + 1
+      return
+      end
+c---------------------------------------------------------
+      subroutine GLOBE_index(xlat,xlon,ix,iy,dx,dy)
+c          given: (xlat,xlon)
+c          find:  (ix,iy) of lower left corner of cell containing (xlat,xlon)
+c                         ix will range from [0 to 43199].
+c                             0 = -179.99583 longitude
+c                         iy will range from [0 to 21599].
+c                             0 = -89.99583
+c                 (dx,dy) is used to interpolate. It is the fraction of cell
+c                         where (xlat,xlon) is located.
+c          Note: Globe data base cell size = 30 seconds = 1/120 degree = .0083333
+      real*8 dlat,dlon,x,y
+      dlat=xlat+90.D0
+      y=dlat*120.D0 -.5D0
+      iy=y
+      dlon=xlon
+      if(xlon.lt.0.) dlon=dlon+360.D0
+      if(dlon.ge.180.D0) then
+	 x=(dlon-180.D0)*120.D0 - .5D0
+	 if(x.lt.0.D0) x=x+43200.D0
+      else
+	 x=(dlon+180.D0)*120.D0 - .5D0
+      end if
+      ix=x
+      dx=x-dfloat(ix)
+      dy=y-dfloat(iy)
+      if(dy.lt.0.D0) then                  !  South Pole flag
+	 iy=0
+	 dy=0.
+      end if
+      return
+      end
+c---------------------------------------------------------
+      function GLOBE_interp(dx,dy,z)
+c          bilinear interpolation routine
+      dimension z(4)
+      z12=z(1) + (z(2)-z(1))*dy
+      z43=z(4) + (z(3)-z(4))*dy
+      zp=z12 + (z43-z12)*dx
+      GLOBE_interp=zp
+      return
+      end
+c-------------------------------------------------------
